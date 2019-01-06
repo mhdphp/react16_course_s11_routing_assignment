fd12dbf5db3fc32a5d3d3a8f3999a13ba0f12539
Mihai Enuica <mihai.enuica@gmail.com>
2019-01-06 09:37:26 +0200

    Assignment part #1 -- 1. Added routes to load "Users" and "Courses" 2. Add simple navigation with two links 3. Make the course clickable and load in "Courses" place - the same route / link 4. Pass the course ID to the Course page //	modified:   public/manifest.json //	new file:   src/App.css //	modified:   src/App.js //	new file:   src/components/AssignmentDescription/AssignmentDescription.js //	modified:   src/containers/Course/Course.js //	modified:   src/containers/Courses/Courses.js

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Course extends Component {
     render () {
         return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+            <div style={this.props.courseStyle}>
+                <h1>{this.props.title}</h1>
+                <p>You selected the Course with ID:{this.props.courseId}</p>
             </div>
         );
     }
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Course from '../Course/Course';
 import './Courses.css';
 
 class Courses extends Component {
@@ -8,19 +9,48 @@ class Courses extends Component {
             { id: 1, title: 'Angular - The Complete Guide' },
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
-        ]
+        ],
+        selectedCourse: null,
+        show: false
+    }
+
+    showCourseHandler = (id) => {
+        const course = {...this.state.courses[id-1]};
+        this.setState({
+            show: true,
+            selectedCourse: course
+        });
     }
 
     render () {
+        let courseToDisplay = null;
+        let courses = null;
+
+        if(this.state.selectedCourse){
+            courseToDisplay = <Course 
+                        courseStyle={{margin: '0 auto', color: 'darkgreen'}}
+                        title={this.state.selectedCourse.title}
+                        courseId={this.state.selectedCourse.id}/>
+        } else {
+            courses = (
+                this.state.courses.map( course => {
+                    return (
+                        <article 
+                            className="Course" 
+                            key={course.id}
+                            onClick={() => this.showCourseHandler(course.id)}>
+                            {course.title}
+                        </article>
+                    );    
+                })
+            );
+        }
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
+                    {courses}
+                    {courseToDisplay}
                 </section>
             </div>
         );
